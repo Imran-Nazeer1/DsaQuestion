e3274f5f9d300a8c38d43a43436a87a85f0f004c
Imran-nazeer1 <imranofficial1106@gmail.com>
2026-06-07 15:01:39 +0530

    Add dutch sort algo

--- AARAY3/sort0s1sand2s.cpp
+++ AARAY3/sort0s1sand2s.cpp
@@ -7,6 +7,26 @@ void display (vector<int> v){
     }
     cout<<endl;
 }
+void dutchsort(vector<int>& v){
+    // one pass m sort krna
+    int left=0;
+    int mid=0;
+    int right=v.size()-1;
+    while(mid<right){
+        if(v[mid]==0){
+            swap(v[left],v[mid]);
+            left++;
+            mid++;
+        }
+        else if(v[mid]==2){
+            swap(v[mid],v[right]);
+            right--;
+        }
+        else{
+            mid++;
+        }
+    }
+}
 
 void sort(vector<int>& v){
     int no0=0;
@@ -33,8 +53,9 @@ void sort(vector<int>& v){
     }
 }
 int main (){
-    vector<int> v={2,0,2,1,1,0};
+    vector<int> v={2,0,2,1,1,0,0,0,1,1,2,1,2,0};
     display(v);
-    sort(v);
+    // sort(v);
+    dutchsort(v);
     display(v);
 }
