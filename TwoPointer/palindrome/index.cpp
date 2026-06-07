44d443815e936263b5d48e2a2b7409448f3c4c1b
Imran-nazeer1 <imranofficial1106@gmail.com>
2026-06-07 15:39:09 +0530

    soome more test cases addedd

---
+++ TwoPointer/palindrome/index.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    string s="i am a boy ";
+    int size=s.size();
+    for(int i=0;i<size;i++){
+        cout<<s[i];
+        isalnum(s[i]);
+    }
+    cout<<endl;
+    for(int i=0;i<size;i++){
+        cout<<s[i];
+    }
+
+    // bool yes=isalnum(s);
+}
