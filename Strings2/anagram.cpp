9f871fd65df1fbe8e880673c22ad447e2e4a0b9c
Imran-nazeer1 <imranofficial1106@gmail.com>
2026-06-14 10:03:42 +0530

    anagram

---
+++ Strings2/anagram.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    string s="physicswallah";
+    string t="wallahphysics";
+
+    sort(s.begin(),s.end());
+    sort(t.begin(),t.end());
+    if(s==t) cout<<"True";
+    else{
+        cout<<"false";
+    }
+
+}
